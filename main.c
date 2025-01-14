eb5285d8647322d055764d01e3ebe5906eb51774
Nmesoma Duru <65504077+nmesosphere@users.noreply.github.com>
2025-01-14 11:57:56 -0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,34 @@
+/*
+ * main.c
+ *
+ * Created: 12/29/2024 6:59:53 PM
+ *  Author: Nmesoma :)
+ *
+ * Microcontroller: ATMega328P (Miuzei)
+ */ 
+
+#include <stdio.h>
+#include <avr/io.h>
+
+int main(void)
+{
+	//TODO:: Please write your application code
+	DDRD &= ~(1 << DDD3); // Sets pin 3 (PD3) of microcontroller to 'input'
+	DDRD |= (1 << DDD7); // Sets pin 7 (PD7) of the microcontroller to 'output'
+	
+	// Initializes at 0 (no motion is detected). LED should be 'off'
+	int motionDetected = 0;
+	
+    while(1)
+    {
+		motionDetected = (PIND & (1 << PIND3));
+		if (motionDetected)
+			{ 
+				PORTD |= (1 << PORTD7); // Turns the LED light on 
+				printf("Motion detected\n");
+			} 
+		else 
+			{ PORTD &= ~(1 << PORTD7); } // Turns LED off
+		
+    }
+}
